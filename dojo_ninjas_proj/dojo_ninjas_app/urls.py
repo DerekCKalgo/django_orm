306c09851c68bd92a1e0b0e0fd71b3b1ccca6f90
derekckalgo <derkoman@yahoo.com>
2021-03-14 04:12:53 -0700

    dojo ninjas with template homework

--- dojo_ninjas_proj/dojo_ninjas_app/urls.py
+++ dojo_ninjas_proj/dojo_ninjas_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index),
+    path('create_dojo', views.dojos),
+    path('create_ninj', views.ninj)
 ]
--- dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render, HttpResponse, redirect
-def index(request):
-    return HttpResponse("This!")
+from .models import *
 
+def index(request):
+    context={
+        'all_dojo':dojo.objects.all()
+    }
+    return render(request, "dojo_ninjas.html", context)
+
+def dojos(request):
+    dojo.objects.create(name=request.POST["name"], city=request.POST["city"], state=request.POST["state"], desc=request.POST["desc"])
+    return redirect('/')
+
+def ninj(request):
+    ninjas.objects.create(first_name=request.POST["first name"], last_name=request.POST["last name"], dojo=dojo.objects.get(id=(request.POST["choice"])))
+    return redirect('/')
